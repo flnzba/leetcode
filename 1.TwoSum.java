cb4f4338db9e0caead0c4956d26241d1c4fc530f
Florian Z <64655691+flnzba@users.noreply.github.com>
2024-04-04 20:40:57 +0000

    devcontainer erstellt + aufbau versuch

---
+++ 1.TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Integer nums[] = {2,7,11,15};
+        Integer target = 9;
+
+        for (i:nums[]) {
+            Integer zwErgebnis = target - nums[i];
+            if (zwErgebnis > 0) {
+
+            }
+        }
+    }
+}
