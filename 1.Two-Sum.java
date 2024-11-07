94a3a1b9addd2963067d4dbdd3d1ac5c39e86704
Florian Zeba <64655691+flnzba@users.noreply.github.com>
2024-11-07 11:57:25 +0100

    Create 1.Two-Sum.java

---
+++ 1.Two-Sum.java
@@ -0,0 +1,32 @@
+class TwoSum_1 {
+    int[] nums = { 2, 7, 11, 15 };
+    int target = 9;
+    int[] result;
+
+    // int[] nums = { 3, 2, 4 };
+    // int target = 6;
+
+    // int[] nums = { 3, 3 };
+    // int target = 6;
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[]{};
+
+    }
+
+    public static void main(String[] args) {
+        TwoSum_1 solution = new TwoSum_1();
+        int[] result = solution.twoSum(solution.nums, solution.target);
+
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
